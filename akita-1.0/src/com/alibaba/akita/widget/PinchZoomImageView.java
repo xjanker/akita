90fa3408fc093afab21d08afc3cf06eb438d738a
Justin Yang @ Home <xjanker@gmail.com>
2012-08-08 22:27:54 +0800

    trick to drag when using ViewPager 3

--- akita-1.0/src/com/alibaba/akita/widget/PinchZoomImageView.java
+++ akita-1.0/src/com/alibaba/akita/widget/PinchZoomImageView.java
@@ -202,9 +202,9 @@ public class PinchZoomImageView extends ImageView
                 if (mode == Mode.DRAGING) {
                     // trick to drag when using ViewPager
                     try{
-                        if (getLeft() >= 20 && (stop_x - event.getRawX()) < 0) {
+                        if (getLeft() >= 50 && (stop_x - event.getRawX()) < 0) {
                             getParent().getParent().requestDisallowInterceptTouchEvent(false);
-                        } else if (getRight() <= (((View)getParent()).getWidth() - 20)
+                        } else if (getRight() <= (((View)getParent()).getWidth() - 50)
                                 && (stop_x - event.getRawX()) > 0 ) {
                             getParent().getParent().requestDisallowInterceptTouchEvent(false);
                         } else {
